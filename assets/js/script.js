b8278a5725f95e0c95cfe62232d9b079ae24df56
ByIlsa <byilsa@aukjevanderwal.nl>
2021-03-06 11:17:14 +0000

    Added diffferent API URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 
 // Fetch returns a promise which is kind of a placeholder for the response we are going to get
 
-// fetch('https://jsonplaceholder.typicode.com/posts/2')
+// fetch('https://rickandmortyapi.com/api')
 //     .then(response => response.json())
 //     .then(json => console.log(json))
 
     document.getElementById('getData').addEventListener('click', getData);
 
     function getData(){
-        fetch('https://jsonplaceholder.typicode.com/posts/9')
+        fetch('https://rickandmortyapi.com/api/character/?name=rick&status=alive')
                 .then(response => response.json())
                 .then(json => console.log(json))
     }
