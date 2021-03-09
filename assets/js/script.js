e26e94d5a8fb7c955ca6f75212b2b14113155999
ByIlsa <byilsa@aukjevanderwal.nl>
2021-03-09 10:48:40 +0000

    WIP API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,20 @@
 
     document.getElementById('getData').addEventListener('click', getData);
 
-    function getData(){
-        fetch('https://rickandmortyapi.com/api/character/?name=rick&status=alive')
-                .then(response => response.json())
-                .then(json => console.log(json))
+    // function getData(){
+    //     fetch('https://rickandmortyapi.com/api/character/?name=rick&status=alive')
+    //             .then(response => response.json())
+    //             .then(json => console.log(json))
+    // }
+
+        function getData(){
+        fetch('https://rickandmortyapi.com/api/character')
+                .then((response) => response.json(data))
+                .then((data) => {
+                    data.forEach(character => console.log(character)); 
+                    // {
+                    //     data += '<div> <h3> ${character}</h3></div>';
+                    // // });
+                    // document.getElementById('result').innerHTML=result
+                })
     }
